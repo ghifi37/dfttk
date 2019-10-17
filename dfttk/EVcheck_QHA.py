6c41a5143d7bb7c3c13fcb82e4f8727ea39949dc
ghifi37 <ghifi37@qq.com>
2019-10-17 13:45:08 -0400

    All static calcs run in EVCheck_QHA.

--- dfttk/EVcheck_QHA.py
+++ dfttk/EVcheck_QHA.py
@@ -3,15 +3,16 @@
 import math
 import numpy as np
 from atomate.vasp.database import VaspCalcDb
-from dfttk.utils import sort_x_by_y, mark_adopted, consistent_check_db
+from dfttk.utils import sort_x_by_y, mark_adopted, consistent_check_db, check_relax_path
 from itertools import combinations
 from pymatgen.analysis.eos import EOS
 from fireworks import FiretaskBase, LaunchPad, Workflow, Firework
 from fireworks.utilities.fw_utilities import explicit_serialize
 from dfttk.input_sets import RelaxSet, StaticSet, ForceConstantsSet
-from dfttk.fworks import StaticFW, OptimizeFW, PhononFW
+from dfttk.fworks import OptimizeFW, StaticFW, PhononFW
 from dfttk.ftasks import QHAAnalysis
 from dfttk.analysis.quasiharmonic import Quasiharmonic
+
 
 @explicit_serialize
 class EVcheck_QHA(FiretaskBase):
@@ -27,10 +28,11 @@ class EVcheck_QHA(FiretaskBase):
     eos_fit: eos fitting
     
     '''
-    _fw_name = 'EVcheck_QHA'
-    required_params = ['db_file', 'tag', 'structure', 'vasp_cmd', 'metadata']
-    optional_params = ['run_num', 'tolerance', 'threshold', 'del_limited', 'vol_spacing', 't_min',
-                       't_max', 't_step', 'phonon', 'phonon_supercell_matrix', 'verbose']
+    _fw_name = 'EVcheck'
+    required_params = ['db_file', 'tag', 'vasp_cmd', 'metadata']
+    optional_params = ['deformations', 'relax_path', 'run_num', 'tolerance', 'threshold', 'del_limited', 'vol_spacing', 't_min',
+                       't_max', 't_step', 'phonon', 'phonon_supercell_matrix', 'verbose', 'modify_incar_params',
+                       'modify_kpoints_params', 'Pos_Shape_relax', 'symmetry_tolerance']
     
     def run_task(self, fw_spec):
         ''' 
@@ -38,36 +40,63 @@ class EVcheck_QHA(FiretaskBase):
             only for internal usage;
 
         Important args:
-        tolerance: acceptable value for average RMS, recommend >= 0.001;
-        threshold: total point number above the value should be reduced, recommend < 16;
+        tolerance: acceptable value for average RMS, recommend >= 0.005;
+        threshold: total point number above the value should be reduced, recommend < 16 or much time to run;
         del_limited: maximum deletion ration for large results;
         vol_spacing: the maximum ratio step between two volumes, larger step will be inserted points to calculate;
         '''
         max_run = 10
+        deformations = self.get('deformations') or []
         db_file = self['db_file']
         tag = self['tag']
-        structure = self['structure']
         vasp_cmd = self['vasp_cmd']
         metadata = self['metadata']
+        relax_path = self['relax_path'] or ''
         run_num = self.get('run_num') or 0
         tolerance = self.get('tolerance') or 0.005
         threshold = self.get('threshold') or 14
         del_limited = self.get('del_limited') or 0.3
-        vol_spacing = self.get('vol_spacing') or 0.02
+        vol_spacing = self.get('vol_spacing') or 0.03
         t_min = self.get('t_min') or 5 
         t_max = self.get('t_max') or 2000
         t_step = self.get('t_step') or 5
         phonon = self.get('phonon') or False
         phonon_supercell_matrix = self.get('phonon_supercell_matrix') or None
         verbose = self.get('verbose') or False
+        modify_incar_params = self.get('modify_incar_params') or {}
+        modify_kpoints_params = self.get('modify_kpoints_params') or {}
+        Pos_Shape_relax = self.get('Pos_Shape_relax') or False
+        symmetry_tolerance = self.get('symmetry_tolerance') or None
         run_num += 1
         
-        if not consistent_check_db(db_file, tag):
-            print('Please check DB, DFTTK running ended!')
+        relax_path, Pos_Shape_relax = check_relax_path(relax_path, db_file, tag, Pos_Shape_relax)
+        if relax_path != '':
+            from pymatgen.io.vasp.inputs import Poscar
+            poscar = Poscar.from_file(relax_path + '/CONTCAR')
+            structure = poscar.structure
+        else:
+            print('''
+#######################################################################
+#                                                                     #
+#     Cannot find relax path for appending calculations, exit!        #
+#               You can modify the tag and run again!                 #
+#                                                                     #
+#######################################################################
+                ''')
             return
+        
+        if phonon:
+            if not consistent_check_db(db_file, tag):
+                print('Please check DB, DFTTK running ended!')
+                return
 
         volumes, energies, dos_objs = self.get_orig_EV(db_file, tag)
-        self.check_points(db_file, metadata, tolerance, threshold, del_limited, volumes, energies, verbose)
+        vol_adds = self.check_deformations_in_volumes(deformations, volumes, structure.volume)
+        if (len(vol_adds)) == 0:
+            self.check_points(db_file, metadata, tolerance, threshold, del_limited, volumes, energies, verbose)
+        else:
+            self.correct = True
+            self.error = 0
         
         EVcheck_result = {}
         EVcheck_result['append_run_num'] = run_num
@@ -82,30 +111,41 @@ class EVcheck_QHA(FiretaskBase):
 
         if self.correct:
             vol_orig = structure.volume
-            volume, energy, dos_obj = self.gen_volenergdos(self.points, volumes, energies, dos_objs)
-            vol_adds = self.check_vol_coverage(volume, vol_spacing, vol_orig, run_num, 
-                                               energy, structure, dos_obj, phonon, 
-                                               db_file, tag, t_min, t_max, t_step)   # Normalized to 1
-            EVcheck_result['sellected'] = volume
-            EVcheck_result['append'] = str(vol_adds * vol_orig)
-            # Marked as adopted in db
-            mark_adopted(tag, db_file, volume)
+            if (len(vol_adds)) == 0:
+                volume, energy, dos_obj = self.gen_volenergdos(self.points, volumes, energies, dos_objs)
+                vol_adds = self.check_vol_coverage(volume, vol_spacing, vol_orig, run_num, 
+                                                   energy, structure, dos_obj, phonon, 
+                                                   db_file, tag, t_min, t_max, t_step,
+                                                   EVcheck_result)   # Normalized to 1
+                EVcheck_result['sellected'] = volume
+                EVcheck_result['append'] = (vol_adds * vol_orig).tolist()
+                # Marked as adopted in db
+                mark_adopted(tag, db_file, volume)
             lpad = LaunchPad.auto_load()
             fws = []
-            if vol_adds != []:      # VASP calculations need to append
+            if len(vol_adds) > 0:      # VASP calculations need to append
                 if run_num < max_run:
                     # Do VASP and check again
-                    print('Append the volumes of : %s to calculate in VASP!' %(vol_adds * vol_orig))
+                    print('Appending the volumes of : %s to calculate in VASP!' %(vol_adds * vol_orig).tolist())
                     calcs = []
-                    # Full relax
-                    vis = RelaxSet(structure)
-                    full_relax_fw = OptimizeFW(structure, symmetry_tolerance=0.05, job_type='normal', name='Full relax', prev_calc_loc=False, vasp_input_set=vis, vasp_cmd=vasp_cmd, db_file=db_file, metadata=metadata, spec={'_preserve_fworker': True})
-                    fws.append(full_relax_fw)
-                    calcs.append(full_relax_fw)
-                    vis = StaticSet(structure)
+
+                    calcs = []
+                    vis_relax = RelaxSet(structure)
+                    vis_static = StaticSet(structure)
+                    isif = 5 if 'infdet' in relax_path else 4
                     for vol_add in vol_adds:
-                        static = StaticFW(structure, scale_lattice = vol_add, name = 'structure_%.3f-static' %(vol_add * vol_orig), 
-                                          vasp_input_set = vis, vasp_cmd = vasp_cmd, db_file = db_file, metadata = metadata, parents = full_relax_fw)
+                        if Pos_Shape_relax:
+                            ps_relax_fw = OptimizeFW(structure, scale_lattice=vol_add, symmetry_tolerance=None, modify_incar = {'ISIF': isif},
+                                                     job_type='normal', name='Pos_Shape_%.3f-relax' %(vol_add * vol_orig), prev_calc_loc=relax_path, 
+                                                     vasp_input_set=vis_relax, vasp_cmd=vasp_cmd, db_file=db_file, metadata=metadata, Pos_Shape_relax = True,
+                                                     modify_incar_params=modify_incar_params, modify_kpoints_params = modify_kpoints_params,
+                                                     parents=None)
+                            fws.append(ps_relax_fw)
+                            static = StaticFW(structure, name = 'structure_%.3f-static' %(vol_add * vol_orig), vasp_input_set=vis_static, vasp_cmd=vasp_cmd, 
+                                              db_file=db_file, metadata=metadata, prev_calc_loc=True, parents=ps_relax_fw)
+                        else:
+                            static = StaticFW(structure, scale_lattice=vol_add, name = 'structure_%.3f-static' %(vol_add * vol_orig), vasp_input_set=vis_static, vasp_cmd=vasp_cmd, 
+                                              db_file=db_file, metadata=metadata, prev_calc_loc=relax_path, parents=None)
                         fws.append(static)
                         calcs.append(static)
                         if phonon:
@@ -116,21 +156,29 @@ class EVcheck_QHA(FiretaskBase):
                                      prev_calc_loc=True, parents=static)
                             fws.append(phonon_fw)
                             calcs.append(phonon_fw)
-                    check_result = Firework(EVcheck_QHA(db_file = db_file, tag = tag, structure = structure, tolerance = tolerance, 
-                                                        threshold = threshold, vol_spacing = vol_spacing, vasp_cmd = vasp_cmd, 
-                                                        metadata = metadata, t_min=t_min, t_max=t_max, t_step=t_step, phonon = phonon,
-                                                        phonon_supercell_matrix = phonon_supercell_matrix, verbose = verbose), 
+                    check_result = Firework(EVcheck_QHA(db_file = db_file, tag = tag, structure = structure, relax_path = relax_path, tolerance = tolerance, 
+                                                        threshold = threshold, vol_spacing = vol_spacing, vasp_cmd = vasp_cmd, run_num = run_num,
+                                                        metadata = metadata, t_min = t_min, t_max = t_max, t_step = t_step, phonon = phonon,
+                                                        phonon_supercell_matrix = phonon_supercell_matrix, symmetry_tolerance = symmetry_tolerance,
+                                                        modify_incar_params = modify_incar_params, verbose = verbose,
+                                                        modify_kpoints_params = modify_kpoints_params), 
                                             parents = calcs, name='%s-EVcheck_QHA' %structure.composition.reduced_formula)
                     fws.append(check_result)
                     strname = "{}:{}".format(structure.composition.reduced_formula, 'EV_QHA_Append')
                     wfs = Workflow(fws, name = strname, metadata=metadata)
+                    if modify_incar_params != {}:
+                        from dfttk.utils import add_modify_incar_by_FWname
+                        add_modify_incar_by_FWname(wfs, modify_incar_params = modify_incar_params)
+                    if modify_kpoints_params != {}:
+                        from dfttk.utils import add_modify_kpoints_by_FWname
+                        add_modify_kpoints_by_FWname(wfs, modify_kpoints_params = modify_kpoints_params)
                     lpad.add_wf(wfs)
                 else:
                     print('''
 
 #######################################################################
 #                                                                     #
-#            Too many times appended VASP running, abort!             #
+#            Too many appended VASP running times, abort!             #
 #                      Please check VASP setting!                     #
 #                                                                     #
 #######################################################################
@@ -183,11 +231,14 @@ class EVcheck_QHA(FiretaskBase):
         energies = []
         volumes = []
         dos_objs = []  # pymatgen.electronic_structure.dos.Dos objects
-        static_calculations = vasp_db.collection.find({'$and':[ {'metadata.tag': tag}, {'adopted': True},
-                                                               {'output.structure.lattice.volume': {'$exists': True} }]})
-        if static_calculations.count() <= 4:
+        if vasp_db.collection.count_documents({'$and':[ {'metadata.tag': tag}, {'adopted': True},
+                                            {'output.structure.lattice.volume': {'$exists': True} }]}) <= 4:
             static_calculations = vasp_db.collection.find({'$and':[ {'metadata.tag': tag},
-                                                               {'output.structure.lattice.volume': {'$exists': True} }]})
+                                                        {'output.structure.lattice.volume': {'$exists': True} }]})
+        else:
+            static_calculations = vasp_db.collection.find({'$and':[ {'metadata.tag': tag}, {'adopted': True},
+                                                        {'output.structure.lattice.volume': {'$exists': True} }]})
+            
         vol_last = 0
         for calc in static_calculations:
             vol = calc['output']['structure']['lattice']['volume']
@@ -251,12 +302,12 @@ class EVcheck_QHA(FiretaskBase):
         # For len(num) > threshold case, do a whole number fitting to pass numbers delete if met tolerance
         for i in range(1):     # To avoid the codes after except running
             if (len(num) > threshold):
-#                try:
-                self.check_fit(volumes, energies)
-#                except:
-#                    if verbose:
-#                        print('Fitting error in: ', comb)
-#                    break
+                try:
+                    self.check_fit(volumes, energies)
+                except:
+                    if verbose:
+                        print('Fitting error in: ', comb, '. If you can not achieve QHA result, try to run far negative deformations.')
+                    break
                 fit_value = self.eos_fit.func(volumes)
                 temperror = 0
                 for m in range(len(volumes)):
@@ -303,12 +354,12 @@ class EVcheck_QHA(FiretaskBase):
                 combination = combinations(comb_source, len_comb)
                 for combs in combination:
                     volume, energy = self.gen_volenerg(combs, volumes, energies)
-#                    try:
-                    self.check_fit(volume, energy)
-#                    except:
-#                        if verbose:
-#                            print('Fitting error in: ', combs)
-#                        continue
+                    try:
+                        self.check_fit(volume, energy)
+                    except:
+                        if verbose:
+                            print('Fitting error in: ', comb, '. If you can not achieve QHA result, try to run far negative deformations.')
+                        continue
                     fit_value = self.eos_fit.func(volume)
                     temperror = 0
                     for m in range(len(volume)):
@@ -331,7 +382,8 @@ class EVcheck_QHA(FiretaskBase):
         
     
     def check_vol_coverage(self, volume, vol_spacing, vol_orig, run_num, energy, structure, 
-                          dos_objects, phonon, db_file, tag, t_min, t_max, t_step):
+                          dos_objects, phonon, db_file, tag, t_min, t_max, t_step,
+                          EVcheck_result):
         result = []
         volumer = volume.copy()
         
@@ -354,6 +406,8 @@ class EVcheck_QHA(FiretaskBase):
                             t_min=t_min, t_max=t_max, t_step=t_step, poisson=0.363615, bp2gru=1)
         vol_max = np.nanmax(qha.optimum_volumes)
         vol_min = np.nanmin(qha.optimum_volumes)
+        EVcheck_result['debye'] = qha.get_summary_dict()
+        EVcheck_result['debye']['temperatures'] = EVcheck_result['debye']['temperatures'].tolist()
         if phonon:
             # get the vibrational properties from the FW spec
             vasp_db = VaspCalcDb.from_db_file(db_file, admin=True)
@@ -367,24 +421,47 @@ class EVcheck_QHA(FiretaskBase):
                                 t_min=t_min, t_max=t_max, t_step=t_step, poisson=0.363615, bp2gru=1)
             vol_max = max(np.nanmax(qha_phonon.optimum_volumes), vol_max)
             vol_min = min(np.nanmax(qha_phonon.optimum_volumes), vol_min)
+            EVcheck_result['phonon'] = qha_phonon.get_summary_dict()
+            EVcheck_result['phonon']['temperatures'] = EVcheck_result['phonon']['temperatures'].tolist()
+        EVcheck_result['MIN_volume_Evaluated'] = '%.3f' %vol_min
+        EVcheck_result['MAX_volume_Evaluated'] = '%.3f' %vol_max
         print('Evaluated MIN volume is %.3f;' %vol_min)
         print('Evaluated MAX volume is %.3f;' %vol_max)
         
         vol_max = vol_max / vol_orig
         vol_min = vol_min / vol_orig
-        counter = 0
+        counter = 1
+        # Using max_append for reducing unnecessary calculations because of rough fitting
+        max_append = 1 if phonon else 2
         # Over coverage ratio set to 1.01 as following
         if volumer[-1] * 1.01 < vol_max:
             result.append(volumer[-1] + vol_spacing)
             # counter is set to limit calculation times when exception occurs
-            while (counter < 4) and (result[-1] < vol_max):
+            while (counter < max_append) and (result[-1] < vol_max):
                 result.append(result[-1] + vol_spacing)
                 counter += 1
-        counter = 0
+        counter = 1
         if volumer[0] * 0.99 > vol_min:
             result.append(volumer[0] - vol_spacing)
-            while (counter < 4) and (result[-1] > vol_min):
+            while (counter < max_append) and (result[-1] > vol_min):
                 result.append(result[-1] - vol_spacing)
                 counter += 1
         return(np.array(result))
+        
+        
+    def check_deformations_in_volumes(self, deformations, volumes, orig_vol):
+        if len(deformations) == 0:
+            return(np.array([]))
+        elif len(volumes) == 0:
+            return(np.array(deformations))
+        else:
+            result = []
+            min_vol = volumes[0] / orig_vol * 0.999
+            max_vol = volumes[-1] / orig_vol * 1.001
+            for deformation in deformations:
+                if deformation < min_vol:
+                    result.append(deformation)
+                if deformation > max_vol:
+                    result.append(deformation)
+            return(np.array(result))
 
